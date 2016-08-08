eaf968f9a7e6e8cca4684b86933534e7638878d7
Paul Mooney <paul@daishisystems.com>
2016-08-08 15:40:09 +0100

    Initial commit

---
+++ WebService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.ServiceFabric.Services.Runtime;
+using WebService;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        try
+        {
+            ServiceRuntime.RegisterServiceAsync("WebServiceType",
+                context => new WebService.WebService(context)).GetAwaiter().GetResult();
+
+            ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(WebService.WebService).Name);
+
+            // Prevents this host process from terminating so services keeps running. 
+            Thread.Sleep(Timeout.Infinite);
+        }
+        catch (Exception e)
+        {
+            ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+            throw;
+        }
+    }
+}
---
+++ WebService/WebService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Description;
+using System.Linq;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace WebService
+{
+    internal sealed class WebService : StatelessService
+    {
+        public WebService(StatelessServiceContext context)
+            : base(context) 
+        { }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            var endpoints = Context.CodePackageActivationContext.GetEndpoints()
+                                   .Where(endpoint => endpoint.Protocol == EndpointProtocol.Http || endpoint.Protocol == EndpointProtocol.Https)
+                                   .Select(endpoint => endpoint.Name);
+
+            return endpoints.Select(endpoint => new ServiceInstanceListener(
+                serviceContext => new OwinCommunicationListener(Startup.ConfigureApp, serviceContext, ServiceEventSource.Current, endpoint), endpoint));
+        }
+    }
+}
