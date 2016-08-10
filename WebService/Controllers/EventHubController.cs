295eff7796604a3ddcdd5a6bf2864710a883bc18
Paul Mooney <paul@daishisystems.com>
2016-08-10 15:15:00 +0100

    Updated Nuget packages

--- WebService/Controllers/EventHubController.cs
+++ WebService/Controllers/EventHubController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
 
 namespace WebService.Controllers
 {
@@ -8,5 +9,18 @@ namespace WebService.Controllers
         {
             return EventHubManager.Instance.IsInitialised && EventHubManager.Instance.IsConnected;
         }
+
+        public HttpResponseMessage Post([FromBody] string shieldEvents)
+        {
+
+            return null;
+        }
+    }
+
+    internal class ShieldEvent
+    {
+        public string IPAddress { get; set; }
+        public string OccurredAt { get; set; }
+        public string PathAndQuery { get; set; }
     }
 }
